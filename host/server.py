71a4b982ab6aa2059a70865e0db6a60e73bb9605
Scott Zhao <zhaomh1998@outlook.com>
2021-07-31 04:19:39 -0400

    server: non-blocking socket; broadcasting UDP packet

--- host/server.py
+++ host/server.py
@@ -1,4 +1,5 @@
 import time
+import json
 import socket
 import argparse
 import traceback
@@ -7,12 +8,22 @@ from decode import decode_tag_packet, decode_tag_packet_bench
 
 import schedule
 
+# https://stackoverflow.com/a/1267524
+my_ip = ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [
+    [(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in
+     [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]])
+assert len(my_ip) >= 1, 'Cannot find own IP Address'
+my_ip = my_ip[0]
+print(f'Server on {my_ip}')
+
+
 class Server:
     def __init__(self, args):
         self.addr = ('0.0.0.0', args.server_port)
 
         # UDP Socket
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         self.sock.setblocking(False)
         self.sock.bind(self.addr)
 
@@ -21,7 +32,20 @@ class Server:
         self.counter_last = self.counter.copy()
         self.last_stat = time.time()
 
+        self.job_mac = None
+        self.job_ping = None
         self.job_stat = schedule.every(2).seconds.do(self.statistics)
+
+    def set_mac_interval(self, interval_s):
+        if self.job_mac is not None:
+            schedule.cancel_job(self.job_mac)
+        self.job_mac = schedule.every(interval_s).seconds.do(self.mac_sync)
+
+    def set_ping_interval(self, interval_s):
+        if self.job_ping is not None:
+            schedule.cancel_job(self.job_ping)
+        self.job_ping = schedule.every(interval_s).seconds.do(self.ping)
+
     def start(self):
         print('Server listening on %s:%s' % self.addr)
         while True:
@@ -66,6 +90,9 @@ class Server:
     def inbound(self, buf_size=4096, verbose=True):
         try:
             data, client_addr = self.sock.recvfrom(buf_size)
+            if client_addr[0] == my_ip:
+                # Ignore own packet
+                return False, None, None
         except KeyboardInterrupt:
             raise
         except BlockingIOError:
@@ -78,6 +105,7 @@ class Server:
 
         try:
             packet = data.decode('ascii')
+            print('[%s:%s] ' % client_addr + packet)
         except UnicodeDecodeError:
             tag_data = decode_tag_packet(data)
             with open('rx.log', 'a') as f:
@@ -89,6 +117,18 @@ class Server:
         if verbose:
             print('[%s:%s] ' % client_addr + packet)
         return True, client_addr, packet
+
+    def ping(self):
+        pak = {
+            'cmd': 'ping'
+        }
+        self.sock.sendto(json.dumps(pak).encode('ascii'), ('255.255.255.255', 8000))
+
+    def mac_sync(self):
+        pak = {
+            'cmd': 'sync'
+        }
+        self.sock.sendto(json.dumps(pak).encode('ascii'), ('255.255.255.255', 8000))
 
 
 def main():
