b20479e0840114c308cdee3e70959b3a2effac69
Scott Zhao <zhaomh1998@outlook.com>
2021-08-01 05:11:39 -0400

    server: add MAC operations; clean up

--- host/server.py
+++ host/server.py
@@ -18,8 +18,8 @@ print(f'Server on {my_ip}')
 
 
 class Server:
-    def __init__(self, args):
-        self.addr = ('0.0.0.0', args.server_port)
+    def __init__(self, args, **kwargs):
+        self.addr = ('0.0.0.0', args['server_port'] if isinstance(args, dict) else args.server_port)
 
         # UDP Socket
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -36,33 +36,23 @@ class Server:
         self.job_ping = None
         self.job_stat = schedule.every(2).seconds.do(self.statistics)
 
-    def set_mac_interval(self, interval_s):
-        if self.job_mac is not None:
-            schedule.cancel_job(self.job_mac)
-        self.job_mac = schedule.every(interval_s).seconds.do(self.mac_sync)
+        self.nodes = dict()
 
-    def set_ping_interval(self, interval_s):
-        if self.job_ping is not None:
-            schedule.cancel_job(self.job_ping)
-        self.job_ping = schedule.every(interval_s).seconds.do(self.ping)
+    # -------------------- Server Operations --------------------
 
     def start(self):
         print('Server listening on %s:%s' % self.addr)
         while True:
             try:
                 schedule.run_pending()
-                success, source, msg = self.inbound(verbose=False)
-                if success:
-                    if source in self.counter.keys():
-                        self.counter[source] += 1
-                    else:
-                        self.counter[source] = 1
-
+                self.inbound()
             except KeyboardInterrupt:
                 self.shutdown()
-            except OSError:
-                traceback.print_exc()
-                self.shutdown()
+
+    def shutdown(self):
+        self.sock.close()
+        print('\nServer shutting down')
+        exit()
 
     def statistics(self):
         t_since_last = time.time() - self.last_stat
@@ -73,21 +63,8 @@ class Server:
                 print('[%s:%s] ' % client + f'{rate} Hz | ' + self.new_packet[client])
         self.counter_last = self.counter.copy()
 
-    def shutdown(self):
-        self.sock.close()
-        print('\nServer shutting down')
-        exit()
-
-    def reply(self, packet, sender):
-        try:
-            self.sock.sendto(packet, sender)
-        except KeyboardInterrupt:
-            raise
-        except:
-            traceback.print_exc()
-            print('\n\n^^^^^^^^^\nFailed to send reply. Network issue?')
-
-    def inbound(self, buf_size=4096, verbose=True):
+    def inbound(self, buf_size=4096):
+        # Retrieve packet
         try:
             data, client_addr = self.sock.recvfrom(buf_size)
             if client_addr[0] == my_ip:
@@ -103,32 +80,80 @@ class Server:
             print('\n\n^^^^^^^^^\nFailed to receive packet.')
             return False, None, None
 
+        # Handle packet
         try:
             packet = data.decode('ascii')
-            print('[%s:%s] ' % client_addr + packet)
         except UnicodeDecodeError:
-            tag_data = decode_tag_packet(data)
-            with open('rx.log', 'a') as f:
-                f.write(','.join('{:8.2f}'.format(i) for i in decode_tag_packet_bench(data)))
-                f.write('\n')
-            packet = '\t'.join('{:8.2f}'.format(i) for i in tag_data)
+            # Binary data packet
+            self.handle_binary_packet(client_addr, data)
+            return
 
-        self.new_packet[client_addr] = packet
-        if verbose:
-            print('[%s:%s] ' % client_addr + packet)
-        return True, client_addr, packet
+        try:
+            parsed = json.loads(packet)
+            if 'cmd' in parsed.keys():
+                self.handle_mac_packet(client_addr, parsed)
+            else:
+                self.handle_plain_text_packet(client_addr, packet)
+        except json.decoder.JSONDecodeError:
+            # Plain text non json
+            self.handle_plain_text_packet(client_addr, packet)
+
+    def outbound(self, packet, addr=('255.255.255.255', 8000)):
+        """ Send packets out. If addr not specified, broadcast packet """
+        # Prepare packet
+        if isinstance(packet, dict):
+            packet = json.dumps(packet).encode('ascii')
+        elif isinstance(packet, str):
+            packet = packet.encode('ascii')
+        assert isinstance(packet, bytes)
+
+        try:
+            self.sock.sendto(packet, addr)
+        except KeyboardInterrupt:
+            raise
+        except:
+            traceback.print_exc()
+            print('\n\n^^^^^^^^^\nFailed to send packet. Network issue?')
+
+    # ----------------- Inbound Packet Handlers -----------------
+
+    def handle_mac_packet(self, addr, packet):
+        pass
+
+    def handle_binary_packet(self, addr, data):
+        tag_data = decode_tag_packet(data)
+        with open('rx.log', 'a') as f:
+            f.write(','.join('{:8.2f}'.format(i) for i in decode_tag_packet_bench(data)))
+            f.write('\n')
+        packet = '\t'.join('{:8.2f}'.format(i) for i in tag_data)
+        self.new_packet[addr] = packet
+
+        # Statistics
+        if addr in self.counter.keys():
+            self.counter[addr] += 1
+        else:
+            self.counter[addr] = 1
+
+    def handle_plain_text_packet(self, addr, packet):
+        print('[%s:%s] ' % addr + packet)
+
+    # --------------------------- MAC ---------------------------
 
     def ping(self):
-        pak = {
-            'cmd': 'ping'
-        }
-        self.sock.sendto(json.dumps(pak).encode('ascii'), ('255.255.255.255', 8000))
+        self.outbound({'cmd': 'ping'})
 
     def mac_sync(self):
-        pak = {
-            'cmd': 'sync'
-        }
-        self.sock.sendto(json.dumps(pak).encode('ascii'), ('255.255.255.255', 8000))
+        self.outbound({'cmd': 'sync'})
+
+    def set_mac_interval(self, interval_s):
+        if self.job_mac is not None:
+            schedule.cancel_job(self.job_mac)
+        self.job_mac = schedule.every(interval_s).seconds.do(self.mac_sync)
+
+    def set_ping_interval(self, interval_s):
+        if self.job_ping is not None:
+            schedule.cancel_job(self.job_ping)
+        self.job_ping = schedule.every(interval_s).seconds.do(self.ping)
 
 
 def main():
